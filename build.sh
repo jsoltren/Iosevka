6324ec84dc194b15d214a8a21aec89f21d44ce66
José Hiram Soltren <jsoltren@gmail.com>
2017-06-02 12:17:30 -0500

    add build comments

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Changes:
+# g – prefer cv12, g with no loop
+# 0 – prefer cv15, zero with no slash or dot, though default cv13 is fine really
+# ~ - prefer cv16, high mounted tilde
+# {} – prefer cv36, flat sided braces
+# $ - fine, this is cv39, dollar sign with full line
+# 3 – prefer cv46, banker’s three
+
 make custom-config jose \
 design='cv12 cv13 cv16 cv36 cv39 cv46' \
 upright='cv12 cv13 cv16 cv36 cv39 cv46' \
