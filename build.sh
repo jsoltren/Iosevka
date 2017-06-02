2cc452988d2d01c21697c32943739b84322b5e92
José Hiram Soltren <jsoltren@gmail.com>
2017-06-02 12:18:46 -0500

    Correct syntax error for config set name

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@
 # $ - fine, this is cv39, dollar sign with full line
 # 3 – prefer cv46, banker’s three
 
-make custom-config jose \
+make custom-config set=jose \
 design='cv12 cv13 cv16 cv36 cv39 cv46' \
 upright='cv12 cv13 cv16 cv36 cv39 cv46' \
 italic='cv12 cv13 cv16 cv36 cv39 cv46' \
 oblique='cv12 cv13 cv16 cv36 cv39 cv46'
 
-make -j8 custom jose
+make -j8 custom set=jose
