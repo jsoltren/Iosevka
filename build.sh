943764543dd23f9a50a709ff9427dea34ee8f176
José Hiram Soltren <jsoltren@gmail.com>
2017-06-02 12:31:29 -0500

    tweak the @ one more time

--- build.sh
+++ build.sh
@@ -7,12 +7,14 @@
 # {} – prefer cv36, flat sided braces
 # $ - fine, this is cv39, dollar sign with full line
 # 3 – prefer cv46, banker’s three
-# @ - prefer cv32, at-sign with full tail circle
+# @ - prefer cv31, at-sign with half tail circle
+
+rm build/targets-jose.mk
 
 make custom-config set=jose \
-design='cv12 cv13 cv16 cv36 cv39 cv46 cv32' \
-upright='cv12 cv13 cv16 cv36 cv39 cv46 cv32' \
-italic='cv12 cv13 cv16 cv36 cv39 cv46 cv32' \
-oblique='cv12 cv13 cv16 cv36 cv39 cv46 cv32'
+design='cv12 cv13 cv16 cv36 cv39 cv46 cv31' \
+upright='cv12 cv13 cv16 cv36 cv39 cv46 cv31' \
+italic='cv12 cv13 cv16 cv36 cv39 cv46 cv31' \
+oblique='cv12 cv13 cv16 cv36 cv39 cv46 cv31'
 
 make -j8 custom set=jose
